cd2ed5cda011a81161dd8f380612a45eb22c1462
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-06 17:04:28 +0530

    1st day

---
+++ Demo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  char a = 65, b = 66, c = 67;
+  printf("ASCII value of 65 is %c\n ", a);
+  printf("ASCII value of 66 is %c\n", b);
+  printf("ASCII value of 67 is %c\n", c);
+  
+
+
+
+//student data
+
+int studentId=07;
+int studentAge=19;
+double studentFee=12;
+char studentGrade='B';
+
+printf("Student id %i\n",studentId);
+printf("student age %i\n",studentAge);
+printf("student fee %lf\n",studentFee);
+printf("student grade %c\n",studentGrade);
+return 0;
+}
