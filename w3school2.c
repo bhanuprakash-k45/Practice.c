495b8b8efbea24bb10940bae5186f7604b052ba2
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-14 21:44:20 +0530

    Muunday practice

---
+++ w3school2.c
@@ -0,0 +1,50 @@
+//14-09-2026
+#include <stdio.h>
+
+int main(){
+  const int BirthYear = 1980;
+  printf("Birth Year: %d\n",BirthYear);
+
+//c operators
+    int myNum=50+60;
+    printf("%d\n",myNum);
+
+    //Arithmetic operators
+    int x=10;
+    int y=3;
+
+    printf("+: %d\n",x+y);
+    printf("-: %d\n",x-y);
+    printf("*: %d\n",x*y);
+    printf("/: %d\n",x/y);
+
+    int z=5;
+    ++z;    //Increment by 1
+    printf("++: %d\n",z);
+    int s=4; 
+    --s;    //Decrement by 1
+    printf("--: %d\n",s);
+    //Example
+    int peopleInroom=0;
+    peopleInroom++;
+    peopleInroom++;
+    peopleInroom++;
+    printf("People in room: %d\n",peopleInroom);
+    peopleInroom--;
+    printf("People in room: %d\n",peopleInroom);
+
+    //assignment operator
+    int g=10;
+    g += 5; //addtion assignment
+    printf("%d\n",g);
+    //Example
+    int savings=100;
+    savings += 50;
+    printf("%d\n",savings);
+
+    
+
+
+
+  return 0;
+}
