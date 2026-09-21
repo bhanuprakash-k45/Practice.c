901c709f2ac752e4495ea253d77c32a5135d3235
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-21 23:00:46 +0530

    Monday

---
+++ w3school6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int i=0;
+
+    while(i<5){
+        printf("%d\n",i);
+        i++;
+    }
+    int countdown=3;
+    while (countdown>0){
+        printf("%d\n",countdown);
+        countdown--;
+    }
+    printf("Happy New Year!!\n");
+
+
+
+    return 0;
+}
